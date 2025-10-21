6730becdc7023a057b14c89a74fa90bdacffa9b5
BeBonez <165836637+BeBonez@users.noreply.github.com>
2025-10-21 15:42:26 -0300

    Basic GameProject Setup Part 1
    - Added base folders (_Scenes, Animation, Materials, Prefabs, Scripts, Sprites, Models)
    - Added base Placeholders with naming conventions
    - Used PlaceHolder materials for yet-to-setup folders.
    - Added Canvas with screen scaling (will tweak UI later)
    - Added base packages (cinemachine, DoTween, 2D Sprite)
    - Added Game Manager with important comment

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    /* The Game Manager is responsibel for:
+     * Defining current GameState, such as Gameplay, Paused, Cutscene, and other activities;
+     * Defining current WorldState, such as Game Mode, Current Save File, and General Progression.
+    */
+}
